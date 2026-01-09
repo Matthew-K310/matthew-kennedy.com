22c42c985fb8fa63a3cde7cebcaa87139483be75
Matthew-K310 <it@matthew-kennedy.com>
2026-01-09 13:16:15 -0600

    dev: Remove port from deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,8 +5,8 @@ set -e
 
 # Check if commit hash is passed as an argument
 if [ -z "$1" ]; then
-  echo "Usage: $0 <commit-hash>"
-  exit 1
+	echo "Usage: $0 <commit-hash>"
+	exit 1
 fi
 
 COMMIT_HASH=$1
@@ -14,42 +14,42 @@ RELEASES_DIR="/home/deploy/releases"
 DEPLOY_BIN="/home/deploy/production/website"
 SERVICE_NAME="site"
 BINARY_NAME="website-${COMMIT_HASH}"
-declare -a PORTS=("3000" "3001" "3002")
+declare -a PORTS=("3000" "1313")
 
 # Check if the binary exists
 if [ ! -f "${RELEASES_DIR}/${BINARY_NAME}" ]; then
-  echo "Binary ${BINARY_NAME} not found in ${RELEASES_DIR}"
-  exit 1
+	echo "Binary ${BINARY_NAME} not found in ${RELEASES_DIR}"
+	exit 1
 fi
 
 # Keep a reference to the previous binary from the symlink
 if [ -L "${DEPLOY_BIN}" ]; then
-  PREVIOUS=$(readlink -f $DEPLOY_BIN)
-  echo "Current binary is ${PREVIOUS}, saved for rollback."
+	PREVIOUS=$(readlink -f $DEPLOY_BIN)
+	echo "Current binary is ${PREVIOUS}, saved for rollback."
 else
-  echo "No symbolic link found, no previous binary to backup."
-  PREVIOUS=""
+	echo "No symbolic link found, no previous binary to backup."
+	PREVIOUS=""
 fi
 
 rollback_deployment() {
-  if [ -n "$PREVIOUS" ]; then
-    echo "Rolling back to previous binary: ${PREVIOUS}"
-    ln -sfn "${PREVIOUS}" "${DEPLOY_BIN}"
-  else
-    echo "No previous binary to roll back to."
-  fi
+	if [ -n "$PREVIOUS" ]; then
+		echo "Rolling back to previous binary: ${PREVIOUS}"
+		ln -sfn "${PREVIOUS}" "${DEPLOY_BIN}"
+	else
+		echo "No previous binary to roll back to."
+	fi
 
-  # wait to restart the services
-  sleep 10
+	# wait to restart the services
+	sleep 10
 
-  # Restart all services with the previous binary
-  for port in "${PORTS[@]}"; do
-    SERVICE="${SERVICE_NAME}@${port}.service"
-    echo "Restarting $SERVICE..."
-    sudo systemctl restart $SERVICE
-  done
+	# Restart all services with the previous binary
+	for port in "${PORTS[@]}"; do
+		SERVICE="${SERVICE_NAME}@${port}.service"
+		echo "Restarting $SERVICE..."
+		sudo systemctl restart $SERVICE
+	done
 
-  echo "Rollback completed."
+	echo "Rollback completed."
 }
 
 # Copy the binary to the deployment directory
@@ -58,37 +58,38 @@ ln -sf "${RELEASES_DIR}/${BINARY_NAME}" "${DEPLOY_BIN}"
 
 WAIT_TIME=5
 restart_service() {
-  local port=$1
-  local SERVICE="${SERVICE_NAME}@${port}.service"
-  echo "Restarting ${SERVICE}..."
+	local port=$1
+	# local SERVICE="${SERVICE_NAME}@${port}.service"
+	local SERVICE="${SERVICE_NAME}.service"
+	echo "Restarting ${SERVICE}..."
 
-  # Restart the service
-  if ! sudo systemctl restart "$SERVICE"; then
-    echo "Error: Failed to restart ${SERVICE}. Rolling back deployment."
+	# Restart the service
+	if ! sudo systemctl restart "$SERVICE"; then
+		echo "Error: Failed to restart ${SERVICE}. Rolling back deployment."
 
-    # Call the rollback function
-    rollback_deployment
-    exit 1
-  fi
+		# Call the rollback function
+		rollback_deployment
+		exit 1
+	fi
 
-  # Wait a few seconds to allow the service to fully start
-  echo "Waiting for ${SERVICE} to fully start..."
-  sleep $WAIT_TIME
+	# Wait a few seconds to allow the service to fully start
+	echo "Waiting for ${SERVICE} to fully start..."
+	sleep $WAIT_TIME
 
-  # Check the status of the service
-  if ! systemctl is-active --quiet "${SERVICE}"; then
-    echo "Error: ${SERVICE} failed to start correctly. Rolling back deployment."
+	# Check the status of the service
+	if ! systemctl is-active --quiet "${SERVICE}"; then
+		echo "Error: ${SERVICE} failed to start correctly. Rolling back deployment."
 
-    # Call the rollback function
-    rollback_deployment
-    exit 1
-  fi
+		# Call the rollback function
+		rollback_deployment
+		exit 1
+	fi
 
-  echo "${SERVICE}.service restarted successfully."
+	echo "${SERVICE}.service restarted successfully."
 }
 
-for port in "${PORTS[@]}"; do
-  restart_service $port
-done
+# for port in "${PORTS[@]}"; do
+# 	restart_service $port
+# done
 
 echo "Deployment completed successfully."
