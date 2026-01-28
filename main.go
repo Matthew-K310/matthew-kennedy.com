59dd1ed28a87f289e3a7c6524a84bea41c4fe0ad
Matthew-K310 <it@matthew-kennedy.com>
2026-01-28 11:49:51 -0600

    i hope this fixes it

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"net/http"
 
-	"github.com/Matthew-K310/matthew-kennedy/pages"
 	"github.com/a-h/templ"
+
+	"github.com/Matthew-K310/matthew-kennedy/pages"
 )
 
+//go:embed static/css/* static/assets/*
+var staticFiles embed.FS
+
 func main() {
-	// Assuming you have a Home() function in pages package
 	http.Handle("/", templ.Handler(pages.Home()))
 	http.Handle("/now", templ.Handler(pages.Now()))
 	http.Handle("/about", templ.Handler(pages.About()))
@@ -17,9 +22,9 @@ func main() {
 	http.Handle("/photos", templ.Handler(pages.Photos()))
 	http.Handle("/music", templ.Handler(pages.Music()))
 
-	// Serve files from ./static at /static/
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	// Serve embedded static files at /static/
+	staticFS, _ := fs.Sub(staticFiles, "static")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(staticFS))))
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
