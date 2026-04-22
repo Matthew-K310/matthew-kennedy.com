d457476929c1cdd54e8260fba9c36914137a51d7
Matthew-K310 <it@matthew-kennedy.com>
2026-04-22 12:21:28 -0500

    big refactor

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
-module github.com/Matthew-K310/matthew-kennedy
+module codeberg.org/Matthew-K310/matthew-kennedy
 
 go 1.25.4
 
 require (
-	github.com/a-h/templ v0.3.960
+	github.com/a-h/templ v0.3.1001
 	github.com/niklasfasching/go-org v1.9.1
 )
 
--- main.go
+++ main.go
@@ -9,33 +9,35 @@ import (
 
 	"github.com/a-h/templ"
 
-	"github.com/Matthew-K310/matthew-kennedy/internal/micro"
-	"github.com/Matthew-K310/matthew-kennedy/pages"
+	//	"github.com/Matthew-K310/matthew-kennedy/internal/micro"
+	"codeberg.org/Matthew-K310/matthew-kennedy/pages"
 )
 
 //go:embed static/css/* static/assets/* content/micro.org
 var staticFiles embed.FS
 
 func main() {
-	posts, err := micro.LoadPosts(staticFiles, "content/micro.org")
-	if err != nil {
-		log.Fatalf("failed to load micro posts: %v", err)
-	}
+	// posts, err := micro.LoadPosts(staticFiles, "content/micro.org")
+	// if err != nil {
+	//	log.Fatalf("failed to load micro posts: %v", err)
+	// }
 
 	http.Handle("/", templ.Handler(pages.Home()))
 	http.Handle("/now", templ.Handler(pages.Now()))
 	http.Handle("/about", templ.Handler(pages.About()))
-	http.Handle("/micro", templ.Handler(pages.Micro(posts)))
+	// http.Handle("/micro", templ.Handler(pages.Micro(posts)))
 	http.Handle("/photos", templ.Handler(pages.Photos()))
 	http.Handle("/music", templ.Handler(pages.Music()))
+	http.Handle("/contact", templ.Handler(pages.Contact()))
+	http.Handle("/links", templ.Handler(pages.Links()))
 
 	staticFS, _ := fs.Sub(staticFiles, "static")
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(staticFS))))
 
-	// handle content files (micro.org)
-	contentFS, _ := fs.Sub(staticFiles, "content")
-	http.Handle("/content/", http.StripPrefix("/content/", http.FileServer(http.FS(contentFS))))
+	// handle content files (micro.org, music, other)
+	// contentFS, _ := fs.Sub(staticFiles, "content")
+	// http.Handle("/content/", http.StripPrefix("/content/", http.FileServer(http.FS(contentFS))))
 
 	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
